48082d3820c7e527a5d8d22965e6cc4757a81a0b
Neville Oronni <nevooronni@gmail.com>
2023-09-12 23:24:02 +0300

    chore(foundations)!: added them colors

---
+++ styles/theme/index.ts
@@ -0,0 +1,29 @@
+/**
+ * App Main Colors
+ */
+
+const colors = {
+  // Primary
+  red: '#b23440',
+  darkRed: '#640200',
+  lightGray: '#959090',
+  mediumGray: '#575051',
+  green: '#046465',
+  lightGreen: '#3eb334',
+  black: '#000000',
+
+  // 'hue-green': {
+  //   50: '#ebf9e5',
+  //   100: '#d0e9c5',
+  //   200: '#b4d9a4',
+  //   300: '#96ca81',
+  //   400: '#7aba5e',
+  //   500: '#62A446',
+  //   600: '#387E1B',
+  //   700: '#335924',
+  //   800: '#1d3713',
+  //   900: '#051400',
+  // },
+};
+
+export default colors;
